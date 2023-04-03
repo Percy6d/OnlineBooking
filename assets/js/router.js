81e7e3ea336b2f11010d978e2103435a30679565
Percy6d <116333145+Percy6d@users.noreply.github.com>
2023-04-04 00:41:26 +0400

    adding the routes to the pages

--- assets/js/router.js
+++ assets/js/router.js
@@ -9,7 +9,20 @@ app.config(($routeProvider, $locationProvider) => {
         controller: "login-controller"
     })
     .when("/register", {
-        templateUrl: "views/register.html"
+        templateUrl: "views/register.html",
+        controller: "register-controller"
+    })
+    .when("/about", {
+        templateUrl: "views/about.html",
+        controller: "about-controller"
+    })
+    .when("/contact", {
+        templateUrl: "views/contact.html",
+        controller: "contact-controller"
+    })
+    .when("/services", {
+        templateUrl: "views/services.html",
+        controller: "services-controller"
     })
     .when("/service/car-rental", {
         templateUrl: "test.html"
