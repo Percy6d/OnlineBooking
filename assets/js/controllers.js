a050e6682584291eacf67fd1e06698b1741532e7
Richard Gigi <gigirichardofficial@gmail.com>
2023-04-09 22:03:56 +0100

    Validated Sign up

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,8 +1,9 @@
 app.controller("intro-controller", function($rootScope, $scope){
 
 });
-app.controller("login-controller", function($rootScope, $scope){
-    
+app.controller("register-controller", function($rootScope, $scope){
+    // Creating an empty object to store form details
+    $scope.user = {};
 });
 app.controller("navbarCtrl", function($rootScope, $scope, $location){
     $scope.path = $location.path();
