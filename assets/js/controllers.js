2dc8be6bfe05896224abe4844523057d033dadc5
Richard Gigi <gigirichardofficial@gmail.com>
2023-04-09 22:16:53 +0100

    Validated registeration form

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -4,6 +4,11 @@ app.controller("intro-controller", function($rootScope, $scope){
 app.controller("register-controller", function($rootScope, $scope){
     // Creating an empty object to store form details
     $scope.user = {};
+
+    // Get user details and create an account via API
+    $scope.createUser = () => {
+        console.log($scope.user);
+    }
 });
 app.controller("navbarCtrl", function($rootScope, $scope, $location){
     $scope.path = $location.path();
