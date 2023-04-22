812a79f370dcab894a82c135dfcde55f1b79bf5f
Richard Gigi <gigirichardofficial@gmail.com>
2023-04-22 12:40:13 +0100

    Updated Database

--- josh_online_booking_project.sql
+++ josh_online_booking_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2023 at 01:51 PM
+-- Generation Time: Apr 22, 2023 at 01:39 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,75 @@ SET time_zone = "+00:00";
 --
 -- Database: `josh_online_booking_project`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `timeCreated` datetime NOT NULL,
+  `timeUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commodities`
+--
+
+CREATE TABLE `commodities` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(20) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `categoryID` int(11) NOT NULL,
+  `typeID` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `timeCreated` datetime NOT NULL,
+  `timeUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commodities_images`
+--
+
+CREATE TABLE `commodities_images` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(20) NOT NULL,
+  `commodityID` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `timeAdded` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(20) NOT NULL,
+  `name` varchar(15) NOT NULL,
+  `timeCreated` datetime NOT NULL,
+  `timeUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `uid`, `name`, `timeCreated`, `timeUpdated`) VALUES
+(1, 'XntTaq4g735IHx2WlSGe', 'product', '2023-04-21 17:54:14', '2023-04-21 17:54:14'),
+(5, 'oJunOKArd9HUL8wbis5t', 'service', '2023-04-22 11:04:19', '2023-04-22 11:04:19');
 
 -- --------------------------------------------------------
 
@@ -45,11 +114,44 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `uid`, `firstname`, `lastname`, `emailAddress`, `mobileNumber`, `password`, `isVerified`, `timeCreated`, `timeUpdated`) VALUES
-(7, 'u9p7tWjZD5C8zLKqOGex', NULL, NULL, 'test@user.com', NULL, '$2y$10$PEzwWGaYsZYAO/SqDJV.a.K/JGSwnQRnsyohJKszJtB69bOsRrnV6', 0, '2023-04-14 11:35:13', '2023-04-14 11:35:13');
+(1, 'psKhb0iAHGQ3MDolmu9e', NULL, NULL, 'gigirichardofficial@gmail.com', NULL, '$2y$10$rT2SEINqJazvVvKwoNiZU.mwrNfS3DetkVu8y5rc/6dCfcTz7LnsK', 0, '2023-04-21 00:27:38', '2023-04-21 00:27:38');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`uid`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `commodities`
+--
+ALTER TABLE `commodities`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`uid`),
+  ADD KEY `categoryID` (`categoryID`),
+  ADD KEY `typeID` (`typeID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `commodities_images`
+--
+ALTER TABLE `commodities_images`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`uid`);
+
+--
+-- Indexes for table `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`uid`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `users`
@@ -63,10 +165,46 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `commodities`
+--
+ALTER TABLE `commodities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `commodities_images`
+--
+ALTER TABLE `commodities_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `commodities`
+--
+ALTER TABLE `commodities`
+  ADD CONSTRAINT `commodities_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `commodities_ibfk_2` FOREIGN KEY (`typeID`) REFERENCES `types` (`id`),
+  ADD CONSTRAINT `commodities_ibfk_3` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
